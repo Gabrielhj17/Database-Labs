521e7fb1faa3ef3b7d0b2950d348f8d3a6d0d6fb
Gabrielhj17 <56936967+Gabrielhj17@users.noreply.github.com>
2024-10-22 14:17:02 +0100

    SQL code for creating the recordLabel table

---
+++ Query.sql
@@ -0,0 +1,10 @@
+CREATE TABLE recordLabel (
+labelName VARCHAR(30) PRIMARY KEY,
+webpage VARCHAR(60),
+house CHAR(6),
+street VARCHAR(50),
+postcode CHAR(8),
+country VARCHAR(30),
+email VARCHAR(50),
+town VARCHAR(25)
+);
